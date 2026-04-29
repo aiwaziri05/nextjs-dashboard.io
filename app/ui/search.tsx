0b6f1a3c918bb4fba0e24eebd13cd4f34add81aa
aiwaziri05 <aiwaziri05.dev@gmail.com>
2026-04-29 14:00:10 +0100

    refactor: optimize search handling with debounced callback

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,17 +2,18 @@
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     term ? params.set('query', term) : params.delete('query');
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className='relative flex flex-1 flex-shrink-0'>
